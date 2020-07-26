24422096a88e5b4189131cd07067aecd1f5be3c3
InfSky-233 <itrident@163.com>
2020-07-26 15:40:40 +0800

    v0.1

---
+++ index.js
@@ -0,0 +1,38 @@
+// ---------------------- swiper设置 -----------------------
+var mySwiper = new Swiper(".swiper-container", {
+  // 循环模式选项
+  loop: true,
+  // 是否自动切换
+  autoplay: {
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // 如果需要分页器
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  // 如果需要前进后退按钮
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+//鼠标滑过pagination控制swiper切换
+for (i = 0; i < mySwiper.pagination.bullets.length; i++) {
+  mySwiper.pagination.bullets[i].onmouseover = function () {
+    this.click();
+  };
+}
+
+//如果你在swiper初始化后才决定使用clickable，可以这样设置
+mySwiper.params.pagination.clickable = true;
+//此外还需要重新初始化pagination
+mySwiper.pagination.destroy();
+mySwiper.pagination.init();
+mySwiper.pagination.bullets.eq(0).addClass("swiper-pagination-bullet-active");
+
+// ---------------------- swiper设置结束 -----------------------
